97f5faffc01382ea172f5029a49238b31e45d4da
fabiha.tasneem <fabiha@synesisit.info>
2024-10-24 11:26:34 +0600

    convert twitter to X

--- src/components/details-card/index.tsx
+++ src/components/details-card/index.tsx
@@ -22,7 +22,7 @@ import {
 import { FaSquareThreads } from 'react-icons/fa6';
 import { MdLocationOn } from 'react-icons/md';
 import { RiMailFill, RiPhoneFill } from 'react-icons/ri';
-import { SiResearchgate, SiTwitter, SiUdemy } from 'react-icons/si';
+import { SiResearchgate, SiX, SiUdemy } from 'react-icons/si';
 import { Profile } from '../../interfaces/profile';
 import {
   SanitizedGithub,
@@ -212,8 +212,8 @@ const DetailsCard = ({ profile, loading, social, github }: Props) => {
               )}
               {social?.twitter && (
                 <ListItem
-                  icon={<SiTwitter />}
-                  title="Twitter:"
+                  icon={<SiX />}
+                  title="X:"
                   value={social.twitter}
                   link={`https://twitter.com/${social.twitter}`}
                 />
