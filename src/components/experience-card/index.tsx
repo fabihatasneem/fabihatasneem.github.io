f489f6ce5882d8f9bd3632054e69c572a9899c38
fabiha.tasneem <fabiha@synesisit.info>
2024-10-30 11:19:58 +0600

    fix design

--- src/components/experience-card/index.tsx
+++ src/components/experience-card/index.tsx
@@ -7,25 +7,15 @@ const ListItem = ({
   position,
   company,
   companyLink,
+  isEven,
 }: {
   time: React.ReactNode;
   position?: React.ReactNode;
   company?: React.ReactNode;
   companyLink?: string;
+  isEven: boolean;
 }) => (
-  <li className="mb-5 ml-4">
-    <div
-      className="absolute w-3 h-3 bg-base-300 rounded-full border-2 border-base-100 mt-1.5 z-10"
-      style={{ left: '-6px' }}
-    ></div>
-    <div
-      className="absolute w-3 h-full bg-base-300"
-      style={{
-        left: '-4px',
-        clipPath:
-          'polygon(0 0, 100% 0, 100% 100%, 0 100%, 50% 85%, 0 70%, 50% 55%, 0 40%, 50% 25%, 0 10%)',
-      }}
-    ></div>
+  <li className={`mb-5 ${isEven ? 'ml-auto pl-4' : 'mr-auto pr-4'} w-[45%]`}>
     <div className="my-0.5 text-xs">{time}</div>
     <h3 className="font-semibold">{position}</h3>
     <div className="mb-4 font-normal">
@@ -49,6 +39,7 @@ const ExperienceCard = ({
       array.push(
         <ListItem
           key={index}
+          isEven={index % 2 === 0}
           time={skeleton({
             widthCls: 'w-5/12',
             heightCls: 'h-4',
@@ -62,9 +53,9 @@ const ExperienceCard = ({
         />,
       );
     }
-
     return array;
   };
+
   return (
     <div id="work" className="card shadow-lg compact bg-base-100">
       <div className="card-body">
@@ -80,7 +71,7 @@ const ExperienceCard = ({
           </h5>
         </div>
         <div className="text-base-content text-opacity-60">
-          <ol className="relative border-l border-base-300 border-opacity-30 my-2 mx-4 zigzag-left">
+          <ol className="relative border-l border-base-300 border-opacity-30 my-2 mx-4 flex flex-col">
             {loading ? (
               renderSkeleton()
             ) : (
@@ -88,6 +79,7 @@ const ExperienceCard = ({
                 {experiences.map((experience, index) => (
                   <ListItem
                     key={index}
+                    isEven={index % 2 === 0}
                     time={`${experience.from} - ${experience.to}`}
                     position={experience.position}
                     company={experience.company}
