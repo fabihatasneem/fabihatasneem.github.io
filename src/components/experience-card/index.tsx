fa36bf4d0a29a26e9ec2e298883cd7d607677a20
fabiha.tasneem <fabiha@synesisit.info>
2024-10-30 12:11:43 +0600

    fix design

--- src/components/experience-card/index.tsx
+++ src/components/experience-card/index.tsx
@@ -15,7 +15,23 @@ const ListItem = ({
   companyLink?: string;
   isEven: boolean;
 }) => (
-  <li className={`mb-5 ${isEven ? 'ml-auto pl-4' : 'mr-auto pr-4'} w-[45%]`}>
+  <li
+    className={`mb-2 ${isEven ? 'ml-auto pl-4' : 'mr-auto pr-4'} w-[45%] relative`}
+  >
+    <div
+      className="absolute w-3 h-3 bg-base-300 rounded-full z-10"
+      style={{
+        top: '10px',
+        [isEven ? 'left' : 'right']: '-6.5px',
+      }}
+    />
+    <div
+      className="absolute w-[15px] h-0.5 bg-base-300"
+      style={{
+        top: '14px',
+        [isEven ? 'left' : 'right']: '-15px',
+      }}
+    />
     <div className="my-0.5 text-xs">{time}</div>
     <h3 className="font-semibold">{position}</h3>
     <div className="mb-4 font-normal">
@@ -71,28 +87,31 @@ const ExperienceCard = ({
           </h5>
         </div>
         <div className="text-base-content text-opacity-60">
-          <ol className="relative border-l border-base-300 border-opacity-30 my-2 mx-4 flex flex-col">
-            {loading ? (
-              renderSkeleton()
-            ) : (
-              <Fragment>
-                {experiences.map((experience, index) => (
-                  <ListItem
-                    key={index}
-                    isEven={index % 2 === 0}
-                    time={`${experience.from} - ${experience.to}`}
-                    position={experience.position}
-                    company={experience.company}
-                    companyLink={
-                      experience.companyLink
-                        ? experience.companyLink
-                        : undefined
-                    }
-                  />
-                ))}
-              </Fragment>
-            )}
-          </ol>
+          <div className="relative my-2 mx-4">
+            <div className="absolute left-1/2 transform -translate-x-1/2 w-0.5 h-full bg-base-300" />
+            <ol className="relative flex flex-col -space-y-3">
+              {loading ? (
+                renderSkeleton()
+              ) : (
+                <Fragment>
+                  {experiences.map((experience, index) => (
+                    <ListItem
+                      key={index}
+                      isEven={index % 2 === 0}
+                      time={`${experience.from} - ${experience.to}`}
+                      position={experience.position}
+                      company={experience.company}
+                      companyLink={
+                        experience.companyLink
+                          ? experience.companyLink
+                          : undefined
+                      }
+                    />
+                  ))}
+                </Fragment>
+              )}
+            </ol>
+          </div>
         </div>
       </div>
     </div>
